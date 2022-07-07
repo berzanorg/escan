9cd6c750ea4106a1aabba8d79fa78c1a16b6a8b3
Berzan <berzan@tutanota.com>
2022-07-07 17:50:20 +0300

    add: Deserialize & Serialize attribute macro

--- src/response/mod.rs
+++ src/response/mod.rs
@@ -1,4 +1,7 @@
+use serde::{Deserialize, Serialize};
+
 /// Response is the main struct for Etherscan API responses
+#[derive(Deserialize, Serialize)]
 pub struct Response<T> {
     pub status: String,
     pub message: String,
