38178e926ddcebaaf07d16853bb95b967fcf1b3b
Berzan <berzan@tutanota.com>
2022-07-08 11:25:58 +0300

    add: transactions_by_range

---
+++ src/endpoints/accounts/transactions_by_range.rs
@@ -0,0 +1,47 @@
+use std::error::Error;
+use serde::Deserialize;
+
+use crate::client::Client;
+use crate::response::Response;
+use crate::enums::Sort;
+
+
+impl Client {
+    /// Gets a list of internal transactions performed within a block range
+    pub async fn transactions_by_range(&self, start_block: u32, end_block: u32, page: u32, offset: u32, sort: Sort) -> Result< Vec<Transaction>, Box<dyn Error>> {
+        // Builds the URL
+        let url = format!(
+            "https://api.etherscan.io/api?module=account&action=txlistinternal&startblock={}&endblock={}&page={}&offset={}&sort={}&apikey={}",
+            start_block,
+            end_block,
+            page,
+            offset,
+            sort.to_str(),
+            self.key,
+        );
+        // Returns
+        Ok(self.web.get(url).send().await?.json::<Response<Vec<Transaction>>>().await?.result)
+    }
+}
+
+
+
+// Custom struct
+#[allow(non_snake_case)]
+#[derive(Debug, Deserialize)]
+pub struct Transaction {
+    pub blockNumber: String,
+    pub timeStamp: String,
+    pub hash: String,
+    pub from: String,
+    pub to: String,
+    pub value: String,
+    pub contractAddress: String,
+    pub input: String,
+    pub r#type: String,
+    pub gas: String,
+    pub gasUsed: String,
+    pub traceId: String,
+    pub isError: String,
+    pub errCode: String,
+}
