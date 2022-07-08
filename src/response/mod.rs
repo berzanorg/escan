228081ba7a5a01d634eaf98ff92749c2c186a643
Berzan <berzan@tutanota.com>
2022-07-08 11:19:42 +0300

    remove: folders

--- src/response/mod.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-/// Response is the main struct for Etherscan API responses
-#[derive(Deserialize, Serialize)]
-pub struct Response<T> {
-    pub status: String,
-    pub message: String,
-    pub result: T,
-}
